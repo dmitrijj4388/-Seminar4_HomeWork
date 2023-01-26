daa63a6c4d778a0e035edf33f9c4b8180fe62265
dmitrijj4388 <dmitrijj4388@mail.ru>
2023-01-27 00:34:14 +0300

    Home work on 4 seminar

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+int exp(int number1, int number2)
+{
+  int result = 1;    
+    for (int i = 1; i <= number2; i++)
+    {
+    result = result*number1;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число А и B: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int result = exp(a, b);
+Console.WriteLine($"{a} в степени {b} = {result}");
---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array()
+{
+    int[] Arr = new int[8];
+    for (int i = 0; i < Arr.Length; i++)
+    {
+         Arr[i] = int.Parse(Console.ReadLine()!);
+    }
+    return Arr;
+}
+
+void printArray(int[] array)
+{
+    string outArrey = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        outArrey += array[i];
+        if (i < array.Length -1) outArrey += ", ";
+    }
+    Console.WriteLine(outArrey);
+}
+
+Console.Clear();
+int[] eight = array();
+printArray(eight);
